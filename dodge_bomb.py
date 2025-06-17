d9450db129ebac517d6654dd855f51484b69dabc
c0a240289a <c0a0289a@edu.teu.ac.jp>
2025-06-17 18:10:07 +0900

    コメントについて #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -29,36 +29,36 @@ def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
 
 
 def gameover(screen: pg.Surface) -> None:
-    ba_img = pg.Surface((WIDTH,HEIGHT)) 
+    ba_img = pg.Surface((WIDTH,HEIGHT))  # 空のSurface(ブラックアウト用)
     pg.draw.rect(ba_img,(0,0,0),(0,0,WIDTH,HEIGHT)) 
-    ba_img.set_alpha(100)
+    ba_img.set_alpha(100)  # 透明度(ブラックアウト)
     screen.blit(ba_img,[0,0])
-    gameover_fonto = pg.font.Font(None,80)
-    txt = gameover_fonto.render("Gameover",True,(255,255,255))
+    gameover_fonto = pg.font.Font(None,80)  # フォントの大きさ80
+    txt = gameover_fonto.render("Gameover",True,(255,255,255))  # 「Gameover」のフォント
     txt_rct = txt.get_rect()
-    txt_rct.center = WIDTH/2,HEIGHT/2
+    txt_rct.center = WIDTH/2,HEIGHT/2  # フォントの座標
     screen.blit(txt,txt_rct)
     kkn_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
     kkn_rct = kkn_img.get_rect()
     kkn_rct2 = kkn_img.get_rect()
-    kkn_rct.center = WIDTH/2+170,HEIGHT/2
-    kkn_rct2.center = WIDTH/2-170,HEIGHT/2
-    screen.blit(kkn_img,kkn_rct)
-    screen.blit(kkn_img,kkn_rct2)
-    pg.display.update()
-    time.sleep(5)
+    kkn_rct.center = WIDTH/2+170,HEIGHT/2  # 右側こうかとんの位置を設定
+    kkn_rct2.center = WIDTH/2-170,HEIGHT/2  # 左側こうかとんの位置を設定
+    screen.blit(kkn_img,kkn_rct)  # 泣いてるこうかとんを表示
+    screen.blit(kkn_img,kkn_rct2)  # 泣いているこうかとんを表示
+    pg.display.update() 
+    time.sleep(5)  # 5秒間の表示
 
 
-    def init__bb_imgs() -> tuple[list[pg.Surface], list[int]]:
-        bb_accs = [a for a in range(1,11)]
+    # def init__bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    #     bb_accs = [a for a in range(1,11)]
 
-        for r in range(1,11):
-            bb_img = pg.Surface((20*r,20*r))
-            pg.draw.circle(bb_img,(255,0,0),(10*r,10*r),10*r)
+    #     for r in range(1,11):
+    #         bb_img = pg.Surface((20*r,20*r))
+    #         pg.draw.circle(bb_img,(255,0,0),(10*r,10*r),10*r)
 
     
-    avx = vx*bb_accs[min(tmr//500,9)]
-    bb_img = bb_imgs[min(tmr//500,9)]
+    # avx = vx*bb_accs[min(tmr//500,9)]
+    # bb_img = bb_imgs[min(tmr//500,9)]  # 演習2途中
 
 
 def main():
@@ -68,7 +68,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_imgs, bb_accs = init_bb_imgs()  # 演習2途中
+    # bb_imgs, bb_accs = init_bb_imgs()  # 演習2途中
     bb_img = pg.Surface((20,20))  # 空のSurfaceを作る（爆弾用）
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_img.set_colorkey((0,0,0))
@@ -78,7 +78,6 @@ def main():
     vx,vy = +5,+5  # 爆弾の移動速度
     clock = pg.time.Clock()
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
